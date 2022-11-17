6741b1664706ee42dba6c40ff60b2896bd67d824
JMartin <jakomail@gmail.com>
2022-11-18 00:31:35 +0100

    remove old codes

--- codes.js
+++ codes.js
@@ -48,37 +48,4 @@
         update: function(dt, elevators, floors) {}
 }
 
-
-
 /*****************************************************************************************************************************************/
-
-// WORKS IN LEVELS: 1,2,3,4,5,6,7, 16 (optimize time or movements)
-// SOMETIME WORKS ON: 8,9,10,11.
-// CONTINUE ON: 12.
-{
-    init: function(elevators, floors) {
-        var pisos = floors.length - 1;
-        const queryString = window.location.hash;
-        const nivel = queryString.substring(queryString.indexOf('=')+1);
-        console.log("Nivel: " + nivel);
-
-        log = function() { return console.log.apply(console, arguments); };  // log("molas tu"); 
-        randomPiso = function(currentPiso){ 
-            var res = Math.floor(Math.random() * pisos) + 1;
-            if (res == currentPiso  ) {res = 0}
-            if (res == pisos && (nivel == 2)){res = 0;}
-            return res;                      
-        }     
-
-        for(var i=0;i<elevators.length;i++){
-            let elevator = elevators[i];
-            elevator.on("floor_button_pressed", function(floorNum) {elevator.goToFloor(floorNum);} );
-            elevator.on("idle", function() {
-                if ((nivel == 6 || nivel == 7) && elevator.loadFactor() > 0.8)    {elevator.goToFloor(randomPiso(elevator.currentFloor()));  }
-                if ( nivel != 6 && nivel != 7)                                    {elevator.goToFloor(randomPiso(elevator.currentFloor()));  }
-            });      
-        } 
-    },    
-        update: function(dt, elevators, floors) {}
-}
-
